bbfd3b9b22f594bdfa6ea060e5fe9557ac071c3e
michaelggr <626419365@qq.com>
2026-05-27 06:31:52 +0800

    feat: 安卓统计修复 + 三端QQ群拉起

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,8 +2,9 @@
  * Electron preload 脚本
  * 目前无需暴露任何 API 到渲染进程
  */
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   platform: process.platform,
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
 });
